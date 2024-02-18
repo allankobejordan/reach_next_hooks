edd999d8bf8a398d3156a76f3ab8b347cea79026
Allan Barbosa Wakanda <allankobe2019@hotmail.com>
2024-02-18 17:59:35 -0300

    hook state 01

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+import { useState } from 'react';
+import HookState01 from './components/UseState01';
+export default () => {
+  return(
+    <>
+      <h1>REACT HOOKS</h1>
+      <HookState01/>
+    </>
+  )
+}
