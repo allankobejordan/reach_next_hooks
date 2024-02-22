952248148e1de9a29dab0631ec12499f84427ba0
Allan Barbosa Wakanda <allankobe2019@hotmail.com>
2024-02-21 21:02:21 -0300

    02_hooks aula

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
 
 import { useState } from 'react';
-import HookState01 from './components/UseState01';
+import HookState02 from './components/UseState02';
 export default () => {
   return(
     <>
       <h1>REACT HOOKS</h1>
-      <HookState01/>
+      <HookState02/>
     </>
   )
 }
---
+++ src/components/UseState02.jsx
@@ -0,0 +1,23 @@
+import {useState} from "react";
+export default () =>{
+    const [valor, setValor] = useState(0);
+    function diminuir(){
+        setValor(v => v-1)
+    }
+    function aumentar(){
+        setValor(v => v + 1)
+    }
+
+    function diminuir_valor(a){
+        setValor(v => v -a)
+    }
+    return(
+        <>
+            <h4>useState</h4>
+            <p>Valor:{valor}</p>
+            <button type="button" onClick={()=> diminuir_valor(100)}>Diminuir+</button>
+            <button type="button" onClick={diminuir}>Diminuir</button>
+            <button type="button" onClick={aumentar}>Aumentar</button>
+        </>
+    )
+}
